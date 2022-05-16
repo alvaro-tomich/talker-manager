59df928e9ad2531234b57919f25d5b78191a5a23
Álvaro Ramos <alvaroramos2222@hotmail.com>
2022-05-16 19:58:41 -0300

    fix: fixing errors in routes

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
-app.use('/', require('./routes/index'));
-
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
+
+app.use('/', require('./routes/index'));
 
 app.listen(PORT, () => {
   console.log('Online');
--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,19 @@ const routes = require('express').Router();
 const fs = require('fs');
 
 routes.get('/talker', async (_req, res) => {
-  const data = await JSON.parse(fs.readFileSync('talker.json'));
-  if (data.length === 0) {
-    res.status(200).send([]);
+  const talkers = await JSON.parse(fs.readFileSync('talker.json'));
+  if (talkers.length === 0) {
+    return res.status(200).send([]);
   }
-    res.status(200).json(data);
+    return res.status(200).json(talkers);
 });
 
 routes.get('/talker/:id', async (req, res) => {
-  const data = await JSON.parse(fs.readFileSync('talker.json'));
+  const talkers = await JSON.parse(fs.readFileSync('talker.json'));
   const { id } = req.params;
-  const filteredData = data.find((talker) => talker.id === parseInt(id, 10));
-  if (!filteredData) res.status(404).json({ message: 'Pessoa palestrante não encontrada' }); 
-  res.status(200).json(filteredData);
+  const findTalker = talkers.find((talker) => talker.id === parseInt(id, 10));
+  if (!findTalker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  return res.status(200).json(findTalker);
 });
 
 module.exports = routes;
