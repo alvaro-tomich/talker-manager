a5f00168d95670b8625f8f09e21199db6700fcba
Álvaro Ramos <alvaroramos2222@hotmail.com>
2022-05-16 16:20:51 -0300

    feat:requirement 2 complete

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,12 @@ routes.get('/talker', async (_req, res) => {
     res.status(200).json(data);
 });
 
+routes.get('/talker/:id', async (req, res) => {
+  const data = await JSON.parse(fs.readFileSync('talker.json'));
+  const { id } = req.params;
+  const filteredData = data.find((talker) => talker.id === parseInt(id, 10));
+  if (!filteredData) res.status(404).json({ message: 'Pessoa palestrante não encontrada' }); 
+  res.status(200).json(filteredData);
+});
+
 module.exports = routes;
